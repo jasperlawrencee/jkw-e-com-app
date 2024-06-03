838048fe4378a045c1181ba6d54930363cff64f3
jasperlawrencee <caballerojasper.55555@gmail.com>
2024-06-03 21:33:00 +0800

    (FE)partial website - mao sani

--- app/(landing)/components/navbar.tsx
+++ app/(landing)/components/navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <h1 className='text-m '>{"Expert in Computer | Expect More"}</h1>
         </div>
         <div className="flex text-zinc-800 gap-x-[3.5rem] text-sm font-medium items-center ">
-            <a href="">{"Home"}</a>
+            {/* <a href="">{"Home"}</a>
             <a href="/items">{"Items"}</a>
             <a href="/about">{"About"}</a>
             <a href="/login"><LoginBtn/></a>
@@ -21,7 +21,7 @@ const Navbar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
             </svg>
-            </a>
+            </a> */}
 
             {/* ambot unsaon ning cart icon */}
             {/* <a href=""><link rel="icon" href="icon" sizes=''/></a> */}
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,24 +2,32 @@ import React from 'react'
 import Image from 'next/image'
 import waiting from '../../assets/svgs/waitingInLine.svg'
 import logo from '../../assets/logo.png'
+import Footer from '../(landing)/components/footer'
+import Navbar from '../(landing)/components/navbar'
 
 const Landing = () => {
   return (
-    <div className='flex  w-[100vw] h-[100vh] bg-zinc-800 items-center justify-center px-[5rem]'>
-      <div className="flex w-[100%] items-center justify-between">
-        <div className="text-left space-y-[1rem] w-[100%] h-[100%] p-[2rem]">
-          <div className="flex gap-x-[1rem] w-[100%] items-center">
-            <div className="w-[2rem] h-[2rem]"><Image src={logo} alt='logo' className='flex'/></div>
-            <p>Jasper Kissa Computer Center</p>
+    <div className="w-screen h-screen bg-zinc-800">
+      <div className="flex flex-col justify-between h-[100%]">
+      <Navbar/>
+      <div className='flex items-center justify-center px-[3.5rem]'>
+        <div className="flex w-[100%] items-center justify-between">
+          <div className="text-left space-y-[1rem] w-[100%] h-[100%]">
+            <div className="flex gap-x-[1rem] w-[100%] items-center">
+              <div className="w-[2rem] h-[2rem]"><Image src={logo} alt='logo' className='flex'/></div>
+              <p>Jasper Kissa Computer Center</p>
+            </div>
+            <h1 className='text-5xl leading-none font-bold'>{"Something is cooking"}</h1>
+            <div className="w-[100%]">
+              <p>{"Exciting updates are on the way! We're working hard behind the scenes to bring you something amazing. Check back soon for the latest news and developments. In the meantime, follow us on our social media channels to stay in the loop and be the first to know when we launch."}</p>
+            </div>
           </div>
-          <h1 className='text-5xl leading-none font-bold'>{"Something is cooking"}</h1>
-          <div className="w-[100%]">
-            <p>{"Exciting updates are on the way! We're working hard behind the scenes to bring you something amazing. Check back soon for the latest news and developments. In the meantime, follow us on our social media channels to stay in the loop and be the first to know when we launch."}</p>
+          <div className="md:hidden sm:hidden lg:flex lg:w-[100%] lg:h-[100%]">
+            <Image src={waiting} alt='waiting'/>
           </div>
         </div>
-        <div className="lg:flex w-[100%] h-[100%] md:hidden sm:hidden">
-          <Image src={waiting} alt='waiting'/>
-        </div>
+    </div>
+    <Footer/> 
       </div>
     </div>
   )
